581510c687a056bfd003d5335a860da22a51def4
Markus Kaiserswerth <mkai@sensun.org>
2011-07-08 00:56:11 +0200

    It's a tag, not a filter

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='django-inline-minify',
       version='0.02',
-      description="Django template filter for minifying inline scripts",
+      description="Django template tag for minifying inline scripts",
       long_description="",
       author='Markus Kaiserswerth',
       author_email='mkai@sensun.org',
