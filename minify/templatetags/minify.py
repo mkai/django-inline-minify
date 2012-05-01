7d7ff79804c4861d4c027e43ea11258ab020a5f4
Markus Kaiserswerth <github@sensun.org>
2012-05-01 22:58:22 +0200

    Added support for Dean Edwards' packer, PEP8

--- minify/templatetags/minify.py
+++ minify/templatetags/minify.py
@@ -1,16 +1,38 @@
 from django import template
 from ..lib.jsmin import jsmin
+from ..lib.packer import JavaScriptPacker
 
 register = template.Library()
 
-class MinifyJs(template.Node):
+
+class MinifyJsNode(template.Node):
     def __init__(self, nodelist):
         self.nodelist = nodelist
+
     def render(self, context):
         return jsmin(self.nodelist.render(context))
-        
+
+
+class JsPackerNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        return JavaScriptPacker().pack(self.nodelist.render(context),
+                                       encoding=62, fastDecode=True)
+
+
 @register.tag
 def minify(parser, token):
+    try:
+        tag_name, engine = token.split_contents()
+    except ValueError:
+        engine = 'jsmin'
+
     nodelist = parser.parse(('endminify',))
     parser.delete_first_token()
-    return MinifyJs(nodelist)
+
+    if engine == 'jsmin':
+        return MinifyJsNode(nodelist)
+    elif engine == 'packer':
+        return JsPackerNode(nodelist)
